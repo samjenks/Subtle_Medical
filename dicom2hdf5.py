69bbc217b97254e09b4bfb26cdea6f8e07fe2a7d
Sam <jenkin49@purdue.edu>
2018-09-29 16:38:22 -0400

    built 3d volume and normalized

--- dicom2hdf5.py
+++ dicom2hdf5.py
@@ -1,5 +1,5 @@
 import pydicom
-import numpy
+import numpy as np
 import h5py
 import os
 import argparse
@@ -10,12 +10,29 @@ class Convert(object):
         self.dicom = dicom_path
         self.hdf5 = hdf5_path
         self.json_path = json_path
-        self.dicom_files = []
+        self.dicom_files = {}
+        self.sl_list = []
 
     def find_dicom(self):
         for file in os.listdir(self.dicom):
             if file.endswith(".dcm"):
-                self.dicom_files.append(os.path.join(self.dicom, file))
+                ds = pydicom.dcmread(os.path.join(self.dicom, file))
+                sl = ds.SliceLocation
+                self.dicom_files[str(sl)] = ds
+                self.sl_list.append(float(sl))
+
+
+
+    def build_volume(self):
+        (x, y), length = self.dicom_files[str(self.sl_list[0])].pixel_array.shape, len(self.sl_list)
+        self.sl_list = sorted(self.sl_list)
+        vol = np.empty((length, x, y), np.float32)
+        for slice in range(length):
+            raw = self.dicom_files[str(self.sl_list[slice])].pixel_array
+            normalized = raw
+            vol[slice] = normalized
+
+
 
 
 
@@ -31,3 +48,4 @@ if __name__ == '__main__':
 
     converter = Convert("../subtlemedicalcodingchallenge/dicom_data/01_BreastMriNactPilot/Mr_Breast - 148579/SagittalIR_3DFGRE_3/", "", "")
     converter.find_dicom()
+    converter.build_volume()
