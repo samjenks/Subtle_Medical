2c1347802e9ae2276df087d6975b9cec19d93e9b
Sam <jenkin49@purdue.edu>
2018-09-30 14:17:39 -0400

    added the convert back script and the fixed argsparse, as well as fixed duplicate slices

--- dicom2hdf5.py
+++ dicom2hdf5.py
@@ -20,24 +20,35 @@ class Convert(object):
             if file.endswith(".dcm"):
                 ds = pydicom.dcmread(os.path.join(self.dicom, file))
                 sl = ds.SliceLocation
-                self.dicom_files[str(sl)] = ds
-                self.sl_list.append(float(sl))
+                if str(sl) in self.dicom_files:
+                    self.dicom_files[str(sl)].append(ds)
+                else:
+                    self.dicom_files[str(sl)] = []
+                    self.dicom_files[str(sl)].append(ds)
+                if float(sl) not in self.sl_list:
+                    self.sl_list.append(float(sl))
+
 
     def build_volume(self):
-        (x, y), length = self.dicom_files[str(self.sl_list[0])].pixel_array.shape, len(self.sl_list)
+        (x, y), length = self.dicom_files[str(self.sl_list[0])][0].pixel_array.shape, len(self.sl_list)
         self.sl_list = sorted(self.sl_list)
-        vol = np.empty((length, x, y), np.float32)
+        vol = []
         for slice in range(length):
-            raw = self.dicom_files[str(self.sl_list[slice])].pixel_array
-            normalized = raw/255.0 # normalizing based on range of int values {0,255}, norm func = x-min(x)/(max(x)-min(x))
-            vol[slice] = normalized
-        self.vol = vol
+
+            for version in range(len(self.dicom_files[str(self.sl_list[slice])])):
+
+                raw = self.dicom_files[str(self.sl_list[slice])][version].pixel_array
+                normalized = raw/255.0 # normalizing based on range of int values {0,255}, norm func = x-min(x)/(max(x)-min(x))
+                vol.append(normalized)
+
+        self.vol = np.array(vol, dtype=np.float32)
+
 
     def convert(self):
         #print(self.dicom_files[str(self.sl_list[45])]) #Modality, SeriesDescription, PixelSpacing
-        mod, sd, ps = self.dicom_files[str(self.sl_list[45])].Modality, \
-                      self.dicom_files[str(self.sl_list[45])].SeriesDescription, \
-                      self.dicom_files[str(self.sl_list[45])].PixelSpacing
+        mod, sd, ps = self.dicom_files[str(self.sl_list[45])][0].Modality, \
+                      self.dicom_files[str(self.sl_list[45])][0].SeriesDescription, \
+                      self.dicom_files[str(self.sl_list[45])][0].PixelSpacing
         data = {}
         data['Modality'] = mod
         data['SeriesDescription'] = sd
@@ -51,20 +62,23 @@ class Convert(object):
         hf.close()
 
 
-
-
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-q", "--output-hdf5")
-    parser.add_argument("-i", "--input-dicom")
-    parser.add_argument("-j", "--output-json")
+    # override the default -h for help
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--help",  action="help", help="show this help message and exit")
+    parser.add_argument("-i", "--input-dicom", dest="dicom_input", help="dicom formatted input file")
+    parser.add_argument("-h", "--output-hdf5", dest="hdf5_output", required=True, help="output of hdf5 file")
+    parser.add_argument("-j", "--output-json", dest="json_output", required=True, help="json formatted output file")
 
+    test_args = ['-i', '../subtlemedicalcodingchallenge/dicom_data/01_BreastMriNactPilot/Mr_Breast - 148579/SagittalIR_3DFGRE_3/','-h', 'output_data.h5',  '-j','output.json']
 
+    # args = parser.parse_args()
 
-    args = parser.parse_args()
+    #override with test args array
+    args = parser.parse_args(test_args)
+
     #converter = Convert("../subtlemedicalcodingchallenge/dicom_data/01_BreastMriNactPilot/Mr_Breast - 148579/SagittalIR_3DFGRE_3/", "output_data.h5", 'output.json')
-    converter = Convert(args.input-dicom, args.output-hdf5, args.output-json)
+    converter = Convert(args.dicom_input, args.hdf5_output, args.json_output)
     converter.find_dicom()
     converter.build_volume()
     converter.convert()
--- hdf52dicom.py
+++ hdf52dicom.py
@@ -3,16 +3,15 @@ import numpy as np
 import h5py
 import os
 import random
-import json
 import argparse
 
 
 class Convert(object):
 
-    def __init__(self, dicom_path, hdf5_path, json_path):
+    def __init__(self, dicom_path, hdf5_path, dicom_output):
         self.dicom = dicom_path
         self.hdf5 = hdf5_path
-        self.json_path = json_path
+        self.output_path = dicom_output
         self.dicom_files = {}
         self.sl_list = []
         self.hdf5_file = []
@@ -25,42 +24,51 @@ class Convert(object):
             if file.endswith(".dcm"):
                 ds = pydicom.dcmread(os.path.join(self.dicom, file))
                 sl = ds.SliceLocation
-                self.dicom_files[str(sl)] = ds
-                self.sl_list.append(float(sl))
+                if str(sl) in self.dicom_files:
+                    self.dicom_files[str(sl)].append(ds)
+                else:
+                    self.dicom_files[str(sl)] = []
+                    self.dicom_files[str(sl)].append(ds)
+                if float(sl) not in self.sl_list:
+                    self.sl_list.append(float(sl))
         # get hdf5 file prepped
         hf = h5py.File(self.hdf5, 'r')
         for data in list(hf.keys()):
-            self.hdf5_file = np.array(hf.get(data))
+            self.hdf5_file = np.array(hf.get(data), dtype=np.int16)
 
     def reformat(self):
         self.new_series_id()
 
-        vol = self.build_volume()
         rescaled = (255 * self.hdf5_file)
         vol = rescaled
-
-        #print(self.dicom_files[str(self.sl_list[1])])
+        vol_idx = 0
         for slice in range(len(self.sl_list)):
-            print("slice:", self.sl_list[slice])
-            #print(type(self.dicom_files[str(self.sl_list[slice])].pixel_array))
-            #print(type(vol[slice]))
-            # self.dicom_files[str(self.sl_list[slice])].pixel_array = vol[slice]
-            self.dicom_files[str(self.sl_list[slice])].SeriesInstanceUID = (self.prefix + "." + str(self.series_suffix))
+            for version in range(len(self.dicom_files[str(self.sl_list[slice])])):
+                # sent the dicom np array to writable
+                self.dicom_files[str(self.sl_list[slice])][version].pixel_array.setflags(write=1)
+                # write to copy
+                np.copyto(self.dicom_files[str(self.sl_list[slice])][version].pixel_array, vol[vol_idx])
+                # sent back to unwritable
+                self.dicom_files[str(self.sl_list[slice])][version].pixel_array.setflags(write=0)
 
-            print(self.dicom_files[str(self.sl_list[slice])].SOPInstanceUID)
+                # replace IDs with new versions
+                self.dicom_files[str(self.sl_list[slice])][version].SeriesInstanceUID = self.prefix + "." + str(self.series_suffix)
+                self.dicom_files[str(self.sl_list[slice])][version].SOPInstanceUID = self.prefix + "." + str(self.new_id())
 
+                # write pixel_array to pixel data and same dicom to output path
+                self.dicom_files[str(self.sl_list[slice])][version].PixelData = \
+                    self.dicom_files[str(self.sl_list[slice])][version].pixel_array.tobytes()
 
+                if not os.path.isdir(self.output_path):
+                    os.mkdir(self.output_path)
+                self.dicom_files[str(self.sl_list[slice])][version]. \
+                    save_as(self.output_path + "output" + str(vol_idx) + ".dcm")
 
-    def build_volume(self):
-        (x, y), length = self.dicom_files[str(self.sl_list[0])].pixel_array.shape, len(self.sl_list)
-        self.sl_list = sorted(self.sl_list)
-        vol = np.empty((length, x, y))
-        for slice in range(length):
-            vol[slice] = self.dicom_files[str(self.sl_list[slice])].pixel_array
-        return vol
+                # increase index of hdf5 volume 3d pixel array
+                vol_idx += 1
 
     def new_series_id(self):
-        list1 = self.dicom_files[str(self.sl_list[1])].SeriesInstanceUID.split(".")
+        list1 = self.dicom_files[str(self.sl_list[1])][0].SeriesInstanceUID.split(".")
         self.prefix = '.'.join(list1[:-1])
         self.series_suffix = random.randint(0, 999999999999999999999999999999)
         if str(list1[len(list1)-1]) == self.series_suffix:
@@ -71,8 +79,24 @@ class Convert(object):
         return random.randint(0, 999999999999999999999999999999)
 
 
-
 if __name__ == '__main__':
-    Converter = Convert("../subtlemedicalcodingchallenge/dicom_data/01_BreastMriNactPilot/Mr_Breast - 148579/SagittalIR_3DFGRE_3/", "output_data.h5", 'output.json')
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("--help", action="help", help="show this help message and exit")
+    parser.add_argument("-d", "--input-dicom", dest="dicom_input", help="dicom formatted input file")
+    parser.add_argument("-h", "--output-hdf5", dest="hdf5_output", required=True, help="output of hdf5 file")
+    parser.add_argument("-o", "--output-dicom", dest="dicom_output", required=True, help="json formatted output file")
+
+    test_args = ['-d',
+                 '../subtlemedicalcodingchallenge/dicom_data/01_BreastMriNactPilot/Mr_Breast - 148579/SagittalIR_3DFGRE_3/',
+                 '-h', 'output_data.h5', '-o', 'output/']
+
+    # args = parser.parse_args()
+
+    # override with test args array
+    args = parser.parse_args(test_args)
+
+    Converter = Convert(args.dicom_input, args.hdf5_output, args.dicom_output)
+
+    # Converter = Convert("../subtlemedicalcodingchallenge/dicom_data/01_BreastMriNactPilot/Mr_Breast - 148579/SagittalIR_3DFGRE_3/", "output_data.h5", 'output/')
     Converter.find_files()
-    #Converter.reformat()
+    Converter.reformat()
