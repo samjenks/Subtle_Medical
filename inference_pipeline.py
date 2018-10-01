fae466fba6b12e91dd26d613553c5cdbeec004b8
Sam <jenkin49@purdue.edu>
2018-09-30 20:01:39 -0400

    completed inference pipeline, minus any pre/post processing functions

--- inference_pipeline.py
+++ inference_pipeline.py
@@ -1,6 +1,5 @@
 import collections
 
-
 # Create a namedtuple type as the entries in a job registry
 JobEntry = collections.namedtuple('JobEntry',
                                   'name config preprocess postprocess func')
@@ -31,6 +30,8 @@ class InferencePipeline:
 
         :return: a InferencePipeline object
         '''
+        self.jobs = {}
+
 
     def register(self, job: JobEntry):
         '''Add a job into the registry.
@@ -39,6 +40,16 @@ class InferencePipeline:
 
         :param job: A JobEntry object containing the job to be registered
         '''
+        if job.name in self.jobs:
+            del self.jobs[job.name]
+        else:
+            job_container = {}
+            job_container['config'] = job.config
+            job_container['preprocess'] = job.preprocess
+            job_container['postprocess'] = job.postprocess
+            job_container['func'] = job.func
+            self.jobs[job.name] = job_container
+
 
     def unregister(self, job_name: str):
         '''Remove a job by name from the registry.
@@ -47,6 +58,12 @@ class InferencePipeline:
 
         :param job_name: the name of the job to be removed
         '''
+        try:
+            del self.jobs[job_name]
+        except KeyError:
+            return KeyError
+
+
 
     def is_job_registered(self, job_name: str) -> bool:
         '''Check if a job_name is registered with the pipeline
@@ -54,6 +71,11 @@ class InferencePipeline:
         :param job_name: str, the unique name of the job
         :return: bool, if the job is registered
         '''
+
+        if job_name in self.jobs:
+            return True
+        else:
+            return False
 
     def execute(self, job_name: str, in_dicom_dir: str, out_dicom_dir: str):
         '''Execute a job specified by job_name, with the in_dicom_dir
@@ -64,3 +86,7 @@ class InferencePipeline:
         :param in_dicom_dir: a string, the path to the input DICOM folder
         :param out_dicom_dir: a string, the path to the output DICOM folder
         '''
+        job = self.jobs[job_name]
+        preprossing_out = job['preprocess'](in_dicom_dir)
+        job_out = job['func'](preprossing_out, {'spacing', (3, 3, 1)}, job['config'])
+        job['postprocess'](job_out, out_dicom_dir)
